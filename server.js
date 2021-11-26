c36c17aa915e7d568fc0a5029c3bb2fc1727ac08
Rodrigo Daud <rodrigodaudskeff@gmail.com>
2021-11-26 17:24:51 -0300

    Commit ready for production

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const cors = require('cors')
 const userRouter = require('./routers/user')
 const campaignRouter = require('./routers/campaign')
 const formatsRouter = require('./routers/formats')
+const multer = require('multer')
 
 const app = express()
+
 app.use(cors({
     origin: '*'
 }))
-const port = process.env.PORT
+
+const port = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(userRouter)
@@ -17,12 +20,22 @@ app.use(campaignRouter)
 app.use(formatsRouter)
 
 
+
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  
+
+
 /**
  * Listen to port 3001
  */
 
-app.listen(port,
-    () => {
-        console.log(`Server is running on port ${port}`)
-    })
+app.listen(port, () => console.log(`Server is running on port ${port}`))
 
